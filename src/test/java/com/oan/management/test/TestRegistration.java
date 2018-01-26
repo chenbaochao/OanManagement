7c2ef3101868fee63d1112c46c1cd364c94ced28
Oan Stultjens <oan.stultjens@hotmail.com>
2018-01-26 13:47:09 +0100

    test with mockmvc

---
+++ src/test/java/com/oan/management/test/TestRegistration.java
@@ -0,0 +1,108 @@
+package com.oan.management.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@ActiveProfiles("it")
+@AutoConfigureMockMvc
+@RunWith(SpringJUnit4ClassRunner.class)
+public class TestRegistration {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void submitRegistrationAccountExists() throws Exception {
+        this.mockMvc
+                .perform(
+                        post("/registration")
+                                .with(csrf())
+                                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                                .param("username", "admin")
+                                .param("firstName", "admin")
+                                .param("lastName", "admin")
+                                .param("email", "admin@mail.com")
+                                .param("confirmEmail", "admin@mail.com")
+                                .param("password", "pass")
+                                .param("confirmPassword", "pass")
+                                .param("terms", "off")
+                )
+                .andExpect(model().hasErrors())
+                .andExpect(model().attributeHasFieldErrors("user", "email"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void submitRegistrationPasswordNotMatching() throws Exception {
+        this.mockMvc
+                .perform(
+                        post("/registration")
+                                .with(csrf())
+                                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                                .param("firstName", "admin")
+                                .param("lastName", "admin")
+                                .param("email", "admin2@mail.com")
+                                .param("confirmEmail", "admin2@mail.com")
+                                .param("password", "password")
+                                .param("confirmPassword", "invalid")
+                                .param("terms", "on")
+                )
+                .andExpect(model().hasErrors())
+                .andExpect(model().attributeHasErrors("user"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void submitRegistrationEmailNotMatching() throws Exception {
+        this.mockMvc
+                .perform(
+                        post("/registration")
+                                .with(csrf())
+                                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                                .param("firstName", "admin")
+                                .param("lastName", "admin")
+                                .param("email", "admin3@mail.com")
+                                .param("confirmEmail", "different@mail.com")
+                                .param("password", "password")
+                                .param("confirmPassword", "invalid")
+                                .param("terms", "on")
+                )
+                .andExpect(model().hasErrors())
+                .andExpect(model().attributeHasErrors("user"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void submitRegistrationSuccess() throws Exception {
+        this.mockMvc
+                .perform(
+                        post("/registration")
+                                .with(csrf())
+                                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                                .param("firstName", "admin")
+                                .param("lastName", "admin")
+                                .param("email", "lol@mail.com")
+                                .param("confirmEmail", "lol@mail.com")
+                                .param("password", "password")
+                                .param("confirmPassword", "password")
+                                .param("terms", "on")
+                )
+                .andExpect(redirectedUrl("/registration?success"))
+                .andExpect(status().is3xxRedirection());
+    }
+
+
+
+}
