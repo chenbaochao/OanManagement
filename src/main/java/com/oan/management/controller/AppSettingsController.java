6eafa05a30fce474d9b02227220cf46d2ff85711
Oan Stultjens <oan.stultjens@hotmail.com>
2018-02-05 11:29:16 +0100

    App setting for motivational text works now.

--- src/main/java/com/oan/management/controller/AppSettingsController.java
+++ src/main/java/com/oan/management/controller/AppSettingsController.java
@@ -1,12 +1,14 @@
 package com.oan.management.controller;
 
 import com.oan.management.model.User;
+import com.oan.management.repository.UserRepository;
 import com.oan.management.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -17,17 +19,32 @@ public class AppSettingsController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/appsettings")
     public String getSettings(Model model, Authentication authentication, @RequestParam(value = "motivationText", required = false) String checkbox) {
         User userLogged = userService.findByUser(authentication.getName());
         if (userLogged != null) {
             model.addAttribute("loggedUser", userLogged);
-            if (checkbox == "on") {
-                System.out.println("test yes");
-            } else {
-                System.out.println("test no");
-            }
         }
         return "appsettings";
+    }
+
+    @PostMapping("/appsettings")
+    public String saveSettings(Model model, Authentication authentication, @RequestParam(value = "motivationText", required = false) String checkbox) {
+        System.out.println(checkbox);
+        User userLogged = userService.findByUser(authentication.getName());
+        if (userLogged != null) {
+            model.addAttribute("loggedUser", userLogged);
+        }
+        if (checkbox == null) {
+            userLogged.setMotivationalTaskMessage(false);
+            userRepository.save(userLogged);
+        } else {
+            userLogged.setMotivationalTaskMessage(true);
+            userRepository.save(userLogged);
+        }
+        return "redirect:/appsettings";
     }
 }
