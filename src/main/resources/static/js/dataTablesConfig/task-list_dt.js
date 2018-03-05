4669e79ffdf1ee284a7edd286009315e1528399c
Oan Stultjens <oan.stultjens@hotmail.com>
2018-03-05 22:26:44 +0100

    - Experiment: Added AJAX delete task button, have to fix DataTable jQuery delete

--- src/main/resources/static/js/dataTablesConfig/task-list_dt.js
+++ src/main/resources/static/js/dataTablesConfig/task-list_dt.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('#completedTaskList').DataTable( {
+    var table = $('#completedTaskList').DataTable( {
             "columnDefs": [
                 { "type": "date", "targets": 1 }
             ],
@@ -10,4 +10,21 @@ $(document).ready(function() {
         }
 
     );
+    $('#completedTaskList').on( 'click', '#deleteTask', function () {
+        var task_id = $(this).attr('value');
+        console.log(task_id);
+
+        $.ajax({
+            url: 'task-delete',
+            data: {id: task_id},
+            type: 'GET',
+            success: function (json) {
+                table
+                    .row( $(this).parents('tr') )
+                    .remove()
+                    .draw();
+                    console.log("DELETED");
+            }
+        })
+    } );
 } );
